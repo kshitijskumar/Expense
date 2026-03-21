606e7f7e61f5b38d0e3175152311c2e6ec2242b2
Kshitij Kumar <kshitij@chalo.com>
2026-03-21 14:53:03 +0530

    insert expense data layer

---
+++ composeApp/src/commonMain/kotlin/org/example/project/data/datasource/ExpenseLocalDataSource.kt
@@ -0,0 +1,31 @@
+package org.example.project.data.datasource
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+import org.example.project.db.DatabaseHelper
+import org.example.project.domain.model.AddExpenseInput
+
+class ExpenseLocalDataSource(private val db: DatabaseHelper) {
+    
+    suspend fun insertWithParticipants(input: AddExpenseInput) = withContext(Dispatchers.IO) {
+        db.database.transaction {
+            db.expenseQueries.insertExpense(
+                title = input.title,
+                amount = input.amount,
+                date = input.date,
+                category_id = input.categoryId,
+                notes = input.notes
+            )
+            
+            val expenseId = db.expenseQueries.lastInsertRowId().executeAsOne()
+            
+            input.participantFriendIds.forEach { friendId ->
+                db.expenseParticipantQueries.insertParticipant(
+                    expense_id = expenseId,
+                    friend_id = friendId
+                )
+            }
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/org/example/project/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/di/DataModule.kt
@@ -1,7 +1,11 @@
 package org.example.project.di
 
+import org.example.project.data.datasource.ExpenseLocalDataSource
+import org.example.project.data.repository.ExpenseRepositoryImpl
+import org.example.project.domain.repository.ExpenseRepository
 import org.koin.dsl.module
 
 val dataModule = module {
-    // Data sources and repository implementations will be added here
+    single { ExpenseLocalDataSource(get()) }
+    single<ExpenseRepository> { ExpenseRepositoryImpl(get()) }
 }
